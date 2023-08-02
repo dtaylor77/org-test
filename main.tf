3a0bc6fda854ad223a227dbf4c729ecd41abe2c6
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-08-02 10:44:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = var.aws_profile
+}
 
+
+resource "aws_inspector2_enabler" "activate-inspector" {
+  account_ids    = ["475803957163"]
+  resource_types = ["ECR", "EC2", "LAMBDA"]
+}
+
+resource "aws_inspector2_member_association" "member-account-association" {
+  account_id = ["698314548707", "585431403493", "550897501474"]
+}
