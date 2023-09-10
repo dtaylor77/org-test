d391d3b8f7cfc8806853c416cc6501aebccc1788
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-09-10 10:04:31 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,20 @@ variable "account_id" {
 variable "account_name" {
   default = "dhaval3"
 }
+
+variable "bucket_name" {
+  description = "The name of the new environment specific bucket to be created"
+}
+
+
+variable "profile_name" {
+  description = "The profile_name where remote state file will get created"
+}
+
+variable "region" {
+  description = "The AWS region"
+}
+
+variable "environment" {
+  description = "The environment (dev/staging/prod)"
+}
