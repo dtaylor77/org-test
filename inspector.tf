c4ba8b1f9b79614a2c04a764cf2cef541a6f13f8
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-09-14 15:52:33 -0700

    Update inspector.tf

--- inspector.tf
+++ inspector.tf
@@ -1,4 +1,5 @@
 resource "aws_inspector2_enabler" "aws_inspector" {
-  account_ids    = var.account_id
+ # account_ids    = var.account_id
+  account_ids    = [data.aws_caller_identity.current.account_id]
   resource_types = ["ECR", "EC2", "LAMBDA"]
 }
