c3bcb9a27a1a4307d9fb9ac5fb2952508f3114ed
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-09-14 17:42:52 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,13 +6,13 @@ provider "aws" {
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "project-terraform-remote-state-us-east-1-dev"
-    key    = "project/security/terraform.tfstate"
+    bucket = "dhaval-aquanow-test-tf-state-file-bucket"
+    key    = "security/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 
-resource "aws_s3_bucket" "terraform_remote_state" {
-  bucket = var.bucket_name
-}
+#resource "aws_s3_bucket" "terraform_remote_state" {
+#  bucket = var.bucket_name
+#}
