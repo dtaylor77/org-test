d7fa6a081036360195143a552f19b750df13269c
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-10-06 15:41:57 -0700

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,6 @@
+resource "aws_vpc_peering_connection" "vpc-peering" {
+  peer_owner_id = var.reporting_accountId
+  peer_vpc_id   = var.reporting-vpc-id[var.environment]
+  vpc_id        = vpc-08212d355e511403e
+  peer_region   = "us-east-1"
+}
