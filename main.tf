5f4f1f6b6f31c00af4fb3a56b1636cb0773175c0
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-10-17 17:25:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,25 @@
 #     Name = "Mytf1VPC"
 #   }
 # }
+
+resource "aws_ce_anomaly_monitor" "service_monitor" {
+  name              = "AWSServiceMonitor"
+  monitor_type      = "DIMENSIONAL"
+  monitor_dimension = "SERVICE"
+}
+
+
+resource "aws_ce_anomaly_subscription" "test" {
+  name      = "DAILYSUBSCRIPTION"
+  threshold = 100
+  frequency = "DAILY"
+
+  monitor_arn_list = [
+    aws_ce_anomaly_monitor.service_monitor.arn,
+  ]
+
+  subscriber {
+    type    = "EMAIL"
+    address = "dhavaltailor777@gmail.com"
+  }
+}
