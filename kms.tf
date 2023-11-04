558d69033b18d459589ccd3981d3694b1669a92e
dtaylor77 <dhavaltailor_07@hotmail.com>
2023-11-04 15:14:50 -0700

    add kms key for cloudtrail

---
+++ kms.tf
@@ -0,0 +1,35 @@
+resource "aws_kms_key" "cloudtrail-key" {
+  description             = "Cloudtrail log encryption"
+  deletion_window_in_days = 7
+  key_usage               = "ENCRYPT_DECRYPT"
+}
+
+resource "aws_kms_key_policy" "kms-key-policy" {
+  key_id = aws_kms_key.cloudtrail-key.id
+  policy = jsonencode({
+    Id = "id1"
+    Statement = [
+      {
+        Action = [
+          "GenerateDataKey*",
+          "kms:Decrypt",
+        ]
+        Effect = "Allow"
+        Principal = {
+          "Service" : "cloudtrail.amazonaws.com"
+        }
+        Resource = "*"
+        "Condition" : {
+          "StringEquals" : {
+            "aws:SourceArn" : "arn:aws:cloudtrail:us-east-1:530265417107:trail/niki-trail"
+          },
+          "StringLike" : {
+            "kms:EncryptionContext:aws:cloudtrail:arn" : "arn:aws:cloudtrail:us-east-1:530265417107:trail/niki-trail/*"
+          }
+        }
+        Sid = "Enable KMS Cloudtrail Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
